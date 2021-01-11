917fb1b7a4331771fa5360637cb943a7c8157d1f
220480 <s220480@student.uek.krakow.pl>
2021-01-11 21:46:54 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Running
+
+1. `npm run build`
+
+1. `npm start`
+
+Project will run on port 8080.
+
+# TODO
+
+* Add window resize handler to correctly scale the canvas 
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+	},
+	plugins: [
+		// new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Raster Graphics',
+		}),
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader',
+				],
+			},
+		],
+	},
+};
